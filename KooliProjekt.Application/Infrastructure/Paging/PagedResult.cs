878ffc61c5f842fd9847fb5380a7f67f3e959664
Miles Tails Delta-777 (Samir Cibis) <samircibis2008@gmail.com>
2025-10-31 15:26:38 +0200

    0.5

--- KooliProjekt.Application/Infrastructure/Paging/PagedResult.cs
+++ KooliProjekt.Application/Infrastructure/Paging/PagedResult.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace KooliProjekt.Application.Infrastructure.Paging
 {
-    public class PagedResult<T> : PagedResultBase
+    public class PagedResult<T>
     {
-        public IList<T> Results { get; set; }
+        public IList<T> Items { get; }
+        public int TotalCount { get; }
+        public int Page { get; }
+        public int PageSize { get; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
 
-        public PagedResult()
+        public PagedResult(IList<T> items, int totalCount, int page, int pageSize)
         {
-            Results = new List<T>();
+            Items = items;
+            TotalCount = totalCount;
+            Page = page;
+            PageSize = pageSize;
         }
     }
 }
