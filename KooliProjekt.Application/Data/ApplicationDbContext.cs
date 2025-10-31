7598bbc49614441bb5dcaeb12a9693097513a4e8
Miles Tails Delta-777 (Samir Cibis) <samircibis2008@gmail.com>
2025-10-31 15:24:42 +0200

    0.5
    Panin oma andmed

--- KooliProjekt.Application/Data/ApplicationDbContext.cs
+++ KooliProjekt.Application/Data/ApplicationDbContext.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace KooliProjekt.Application.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
         }
 
-        public DbSet<ToDoList> ToDoLists { get; set; }
-        public DbSet<ToDoItem> ToDoItems { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<WorkLog> WorkLogs { get; set; }
+        public DbSet<ProjectTeam> ProjectTeams { get; set; }
+
     }
 }
