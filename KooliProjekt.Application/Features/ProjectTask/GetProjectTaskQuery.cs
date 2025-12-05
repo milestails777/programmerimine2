3a3a2df5437f2cd0a2b3cc4ec6c101e68d618f45
samir.cibis <samir.cibis@KLASSB314-19>
2025-12-05 13:05:38 +0200

    ver 1.6 NT 4.00.1381

---
+++ KooliProjekt.Application/Features/ProjectTask/GetProjectTaskQuery.cs
@@ -0,0 +1,15 @@
+﻿using KooliProjekt.Application.Infrastructure.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KooliProjekt.Application.Features.ProjectTeams
+{
+    public class GetProjectTaskQuery : IRequest<OperationResult<object>>, IBaseRequest
+    {
+        public int Id { get; set; }
+    }
+}
--- KooliProjekt.WebAPI/Controllers/TasksController.cs
+++ KooliProjekt.WebAPI/Controllers/TasksController.cs
@@ -1,8 +1,60 @@
-namespace KooliProjekt.WebAPI.Controllers
-{
-    public class TasksController : ApiControllerBase
-    {
-        // Your controller actions and methods go here
+using Azure;
+using KooliProjekt.Application.Features.Projects;
+using KooliProjekt.Application.Features.ProjectTeams;
+using KooliProjekt.WebAPI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
+namespace KooliProjekt.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectTaskController : ApiControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProjectTaskController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProjectTasks([FromQuery] ProjectTaskQuery query)
+        {
+            var result = await _mediator.Send(query);
+
+            return Result(result);
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var query = new GetProjectTaskQuery { Id = id };
+            var response = await _mediator.Send(query);
+
+            return Result(response);
+        }
+
+        [HttpPost]
+        [Route("Save")]
+        public async Task<IActionResult> Save(SaveProjectTaskCommand command)
+        {
+            var response = await _mediator.Send(command);
+
+            return Result(response);
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public async Task<IActionResult> Delete(DeleteProjectTaskCommand command)
+        {
+            var response = await _mediator.Send(command);
+
+            return Result(response);
+        }
     }
+
 }
