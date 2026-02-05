e45dfeda1d721e3ef0c4f9cbb5d021849d48c848
Samir Cibis <samir.cibis@tptlive.ee>
2026-02-05 13:05:55 +0200

    1.10 Windows NT 4.01.1381 Beta Version

---
+++ KooliProjekt.Application.UnitTests/Features/ProjectTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KooliProjekt.Application.Data;
+using KooliProjekt.Application.Features.ToDoLists;
+using Xunit;
+
+namespace KooliProjekt.Application.UnitTests.Features
+{
+    public class ProjectTests : TestBase
+    {
+        [Fact]
+        public void Get_throws_if_dbcontext_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new GetProjectsQueryHandler(null);
+            });
+        }
+
+        [Fact]
+        public async Task Get_should_return_object_if_object_exists()
+        {
+            // Arrange
+            var query = new GetProjectsQuery { Id = 1 };
+            var project = new Project { Title = "Test Project" };
+            var handler = new GetProjectsQueryHandler(DbContext);
+            await DbContext.Projects.AddAsync(project);
+            await DbContext.SaveChangesAsync();
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.HasErrors);
+            Assert.NotNull(result.Value);
+            Assert.Equal(1, result.Value.Id);
+        }
+
+        [Fact]
+        public async Task Get_should_return_null_if_object_does_not_exist()
+        {
+            // Arrange
+            var query = new GetProjectsQuery { Id = 101 };
+            var project = new Project { Title = "Test Project" };
+            var handler = new GetProjectsQueryHandler(DbContext);
+            await DbContext.Projects.AddAsync(todoList);
+            await DbContext.SaveChangesAsync();
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.HasErrors);
+            Assert.Null(result.Value);
+        }
+    }
+}
