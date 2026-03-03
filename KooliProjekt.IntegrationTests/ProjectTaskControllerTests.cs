df28b154e705f9370886c9747bb66f408f004061
Samir Cibis <samir.cibis@tptlive.ee>
2026-03-03 14:05:00 +0200

    Windows NT 4.0 IntegrantionTest was added 3.03.2026

---
+++ KooliProjekt.IntegrationTests/ProjectTaskControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using KooliProjekt.Application.Data;
+using KooliProjekt.Application.Infrastructure.Paging;
+using KooliProjekt.Application.Infrastructure.Results;
+using KooliProjekt.IntegrationTests.Helpers;
+using Xunit;
+
+namespace KooliProjekt.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class ProjectTaskControllerTests : TestBase
+    {
+        [Fact]
+        public async Task List_should_return_paged_result()
+        {
+            // Arrange
+            var url = "/api/ProjectTasks/List/?page=0&pageSize=0";
+
+            // Act
+            var response = await Client.GetFromJsonAsync<OperationResult<PagedResult<ProjectTask>>>(url);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.False(response.HasErrors);
+        }
+
+        [Fact]
+        public async Task Get_should_return_list()
+        {
+            // Arrange
+            var url = "/api/ProjectTasks/Get/?id=1";
+            
+            var task = new ProjectTask { Name = "Test Task" };
+            await DbContext.AddAsync(task);
+            await DbContext.SaveChangesAsync();
+
+            // Act
+            var response = await Client.GetFromJsonAsync<OperationResult<ProjectTask>>(url);
+            
+            // Assert
+            Assert.NotNull(response);
+            Assert.False(response.HasErrors);
+        }
+
+        [Fact]
+        public async Task Get_should_return_not_found_for_missing_list()
+        {
+            // Arrange
+            var url = "/api/ProjectTasks/Get/?id=131";
+
+            // Act
+            var response = await Client.GetAsync(url);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
