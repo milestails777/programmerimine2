6790c82991377e28ed1b559c45b85657dea6dba9
Samir Cibis <samir.cibis@tptlive.ee>
2026-04-10 12:56:59 +0300

    Windows NT 4.0 Workstation WPFApplication installation

---
+++ KooliProjekt.WpfApplication/Api/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace KooliProjekt.WpfApplication
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ KooliProjekt.WpfApplication/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KooliProjekt.WpfApplication
+{
+    public class MainWindowViewModel
+    {
+        public IList<Project> Data 
+        { 
+            get
+            {
+                var items = new List<Project>
+                {
+                    new Project { Id = 1, Title = "Test 1" },
+                    new Project { Id = 2, Title = "Test 2" },
+                    new Project { Id = 3, Title = "Test 3" }
+                };
+
+                return items;
+            }
+        }
+
+        public object SelectedItem { get; set; }
+    }
+}
